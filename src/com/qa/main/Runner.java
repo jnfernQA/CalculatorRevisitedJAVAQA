8d59aad38b1134f8d3c5b67921eaba7ad70a1d00
jnfernQA <JFernandes@qa.com>
2021-01-07 12:06:47 +0000

    Added Calculator Revisted Exercise

---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,23 @@
+package com.qa.main;
+
+public class Runner {
+	
+	//Variables
+	static double num1 = 2;
+	static double num2 = 10;
+			
+	//Methods
+	public static void main(String[] args) {
+			
+	//Method Division 
+		
+		if (num1<num2) {
+			double result3 = num1 / num2;
+			System.out.println(result3);
+		}else {
+			System.out.println("Cannot Perform Division Operation");
+		}
+			
+	}
+
+}
